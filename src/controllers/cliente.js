bc436e3bf6001cbb3124427884e0292cc8a39258
OtavioMunhoz <otavio2k20@gmail.com>
2025-04-17 20:40:38 -0300

    aula-17-04

--- src/controllers/cliente.js
+++ src/controllers/cliente.js
@@ -29,11 +29,37 @@ module.exports = {
 
     async cadastrarClientes(request, response) {
         try {
+
+            const { nome, email, celular, endereco } = request.body;
+
+            // instrução SQL
+            const sql = `
+            INSERT INTO CLIENTE 
+                (cli_nome, cli_email, cli_cel, cli_end) 
+            VALUES 
+                (?, ?, ?, ?), `;
+
+            //definição de dados a serem inseridos em um array
+            const values = [ nome, email, celular, endereco];
+
+            // execução da instrução sql passando os parâmetros
+            const [result] = await db.query(sql, values);
+
+            // identificação do ID do registro inserido
+            const dados = {
+                id: result.insertId,
+                nome,
+                email,
+                celular,
+                endereco,
+            }
+
+
             return response.status(200).json({
 
                 sucesso: true,
-                mensagem: 'cadastro de clientes',
-                dados: null
+                mensagem: 'Cadastro de cliente realizado com sucesso!',
+                dados: dados
             });
         }
         catch (error) {
